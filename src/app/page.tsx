0cadfd78449d18315f2ee0b0f80bd77c3a593bd7
jones-bass <jones-bass.tb@gmail.com>
2023-10-28 21:43:40 -0300

    Iniciando Projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
+export default function Home() {
+  return (
+    <div className="min-h-screen px-4 sm:px-6 md:px-8 bg-slate-800">
+      <div>Iniciando Projeto</div>
+    </div>
+  )
+}
